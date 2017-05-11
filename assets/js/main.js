0d4d452e72e0386505cc2f4a4effab68ed5e52d3
“Trinidad <tfuentesfuentealba@gmail.com>
2017-05-11 20:48:53 -0300

    correcciones finales

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,14 +2,14 @@ document.getElementById("toggle").addEventListener("click", function(event){
 	event.preventDefaut();
 	document.getElementById("header").classList.toggle("open");
 	document.getElementById("body").classList.toggle("overflow-hidden")
-})
+});
 
 var lastScrollTop = 0;
 window.addEventListener("scroll", function(){
 	var currentScroll = window.pageYOffset || document.body.scrollTop;
 
 	if (currentScroll >  lastScrollTop) {
-		document.getElementById("nav-header").classList.remove("header-in");
+ 		document.getElementById("nav-header").classList.remove("header-in");
 		document.getElementById("nav-header").style.opacity = 0;
 	} else {
 		document.getElementById("nav-header").classList.add("header-in");
@@ -21,5 +21,5 @@ window.addEventListener("scroll", function(){
 		}
 	}
 	lastScrollTop = currentScroll;
-});
+}, false);
 
